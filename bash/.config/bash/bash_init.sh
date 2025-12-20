8ca34d017308664e69a9e993a526b3bf58ff5532
Alberto Benatti <albi.benni8@gmail.com>
2025-12-20 16:24:41 +0100

    sourced in bashrc already

--- bash/.config/bash/bash_init.sh
+++ bash/.config/bash/bash_init.sh
@@ -9,12 +9,3 @@ fi
 if command -v zoxide &>/dev/null; then
     eval "$(zoxide init bash)"
 fi
-
-if command -v fzf &>/dev/null; then
-    if [[ -f /opt/homebrew/opt/fzf/shell/completion.bash ]]; then
-        source /opt/homebrew/opt/fzf/shell/completion.bash
-    fi
-    if [[ -f /opt/homebrew/opt/fzf/shell/key-bindings.bash ]]; then
-        source /opt/homebrew/opt/fzf/shell/key-bindings.bash
-    fi
-fi
