1ad0263655248d7cc7eea2b562a4a080647dc416
Alberto Benatti <albi.benni8@gmail.com>
2026-01-03 12:43:39 +0100

    cli ai mac conf

---
+++ personal/.config/personal/cli-ai.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+iclaude(){
+    npm install -g @anthropic-ai/claude-code
+}
+
+igemini(){
+    npm install -g @google/gemini-cli
+}
+
+cclaude(){
+    uclaude
+    claude
+}
+
+gem(){
+    gemini
+}
