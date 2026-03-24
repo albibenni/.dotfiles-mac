4f48d7da01eb3ead45cad52960d8fdfcd3cfd8c6
Alberto Benatti <albi.benni8@gmail.com>
2026-03-24 10:53:23 +0100

    updated claude commands

--- personal/.config/personal/cli-ai.sh
+++ personal/.config/personal/cli-ai.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
 iclaude(){
-    npm install -g @anthropic-ai/claude-code
+    # npm install -g @anthropic-ai/claude-code
+    curl -fsSL https://claude.ai/install.sh | bash
 }
 
 igemini(){
     npm install -g @google/gemini-cli
 }
 
-cclaude(){
-    uclaude
+# cclaude(){
+#     iclaude
+#     claude
+# }
+
+cld(){
     claude
 }
 
